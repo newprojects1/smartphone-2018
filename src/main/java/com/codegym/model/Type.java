9f171c9e2f441deba202c49b0bec4acc1f44d5e9
vietson6103@gmail.com <Hn0912388240>
2018-09-20 11:18:25 +0700

    add model Type.java

---
+++ src/main/java/com/codegym/model/Type.java
@@ -0,0 +1,49 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "types")
+public class Type {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(targetEntity = Product.class)
+    private List<Product> products;
+
+    public Type() {
+    }
+
+    public Type(String name, List<Product> products) {
+        this.name = name;
+        this.products = products;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
