dbbd473adcf5ba15f385f68de0529e82f3f1ca28
vietson6103@gmail.com <Hn0912388240>
2018-09-20 11:22:29 +0700

    add repository

---
+++ src/main/java/com/codegym/repository/ProducerRepository.java
@@ -0,0 +1,7 @@
+package com.codegym.repository;
+
+import com.codegym.model.Producer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ProducerRepository extends PagingAndSortingRepository<Producer, Long> {
+}
