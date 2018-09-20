56f168576cbdbe42866536b91f65169ca5ab72f3
vietson6103@gmail.com <Hn0912388240>
2018-09-20 15:42:02 +0700

    add method show list product in class ProductController

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -8,13 +8,13 @@ import com.codegym.service.ProducerService;
 import com.codegym.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.awt.print.Pageable;
 import java.util.Optional;
 
 @Controller
@@ -36,5 +36,19 @@ public class ProductController {
     @ModelAttribute
     public Iterable<Category> categories() {
         return categoryService.findAll();
+    }
+
+    //    show list
+    @GetMapping("/products")
+    public ModelAndView listProducts(@RequestParam("s") Optional<String> s, Pageable pageable) {
+        Page<Product> products;
+        if (s.isPresent()) {
+            products = productService.findAllByNameContainingOrCodeContainingOrProducer(s.get(), s.get(), s.get(), pageable);
+        } else {
+            products = productService.findAll(pageable);
+        }
+        ModelAndView modelAndView1 = new ModelAndView("/product/list");
+        modelAndView1.addObject("products", products);
+        return modelAndView1;
     }
 }
