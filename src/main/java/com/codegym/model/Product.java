0404453569b2651894b6c6712a22618bb08f4008
vietson6103@gmail.com <Hn0912388240>
2018-09-20 11:18:00 +0700

    add model Product.java

---
+++ src/main/java/com/codegym/model/Product.java
@@ -0,0 +1,73 @@
+package com.codegym.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "producer_id")
+    private Producer producer;
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
+    private Type type;
+
+    private int memory;
+
+    private int capacity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Producer getProducer() {
+        return producer;
+    }
+
+    public void setProducer(Producer producer) {
+        this.producer = producer;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public int getMemory() {
+        return memory;
+    }
+
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+}
