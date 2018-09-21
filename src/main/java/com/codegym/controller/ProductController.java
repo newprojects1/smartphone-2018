fa18ccff358d404063a62a199f259e8c2407cba9
vietson6103@gmail.com <Hn0912388240>
2018-09-21 15:46:53 +0700

    fix method create new product in class ProductController

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -3,9 +3,11 @@ package com.codegym.controller;
 import com.codegym.model.Category;
 import com.codegym.model.Producer;
 import com.codegym.model.Product;
+import com.codegym.model.ProductForm;
 import com.codegym.service.CategoryService;
 import com.codegym.service.ProducerService;
 import com.codegym.service.ProductService;
+import com.codegym.utils.StorageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,7 +15,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
+import java.util.UUID;
 
 @Controller
 public class ProductController {
@@ -54,32 +59,41 @@ public class ProductController {
     @GetMapping("/create-product")
     public ModelAndView showCreateForm() {
         ModelAndView modelAndView1 = new ModelAndView("/product/create");
-        modelAndView1.addObject("product", new Product());
+        modelAndView1.addObject("productForm", new ProductForm());
         return modelAndView1;
     }
 
     @PostMapping("/create-product")
-    public ModelAndView saveProduct(@ModelAttribute("product") Product product) {
-        productService.save(product);
-        product.setCode("CG-" + product.getProducer() + "-" + product.getId());
-        productService.save(product);
+    public ModelAndView saveProduct(@ModelAttribute("productForm") ProductForm productForm) {
         ModelAndView modelAndView1 = new ModelAndView("/product/create");
-        modelAndView1.addObject("product", new Product());
+
+        try {
+            String randomCode = UUID.randomUUID().toString();
+            String originFileName = productForm.getImage().getOriginalFilename();
+            String randomName = randomCode + StorageUtils.getFileExtension(originFileName);
+            productForm.getImage().transferTo(new File(StorageUtils.FEATURE_LOCATION + "/" + randomName));
+
+            Product product = new Product();
+            product.setCode(productForm.getCode());
+            product.setName(productForm.getName());
+            product.setCategory(productForm.getCategory());
+            product.setProducer(productForm.getProducer());
+            product.setPrice(productForm.getPrice());
+            product.setMemory(productForm.getMemory());
+            product.setStorage(productForm.getStorage());
+            product.setQuantity(productForm.getQuantity());
+            product.setDetail(productForm.getDetail());
+            product.setImage(randomName);
+
+            productService.save(product);
+            product.setCode("SS-" + product.getId());
+            productService.save(product);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        modelAndView1.addObject("productForm", new ProductForm());
         modelAndView1.addObject("message", "New customer created successfully");
         return modelAndView1;
-    }
-
-    //    edit
-    @GetMapping("/edit-product/{id}")
-    public ModelAndView showEditForm(@PathVariable Long id) {
-        Product product = productService.findById(id);
-        if (product != null) {
-            ModelAndView modelAndView1 = new ModelAndView("/product/edit");
-            modelAndView1.addObject("product", product);
-            return modelAndView1;
-        } else {
-            ModelAndView modelAndView1 = new ModelAndView("/product/error.404");
-            return modelAndView1;
-        }
     }
 }
