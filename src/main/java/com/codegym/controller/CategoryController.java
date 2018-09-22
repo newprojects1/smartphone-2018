0609508adbed974844d515e9577b2d297dde14ea
vietson6103@gmail.com <Hn0912388240>
2018-09-22 10:07:50 +0700

    add class CategoryController

---
+++ src/main/java/com/codegym/controller/CategoryController.java
@@ -0,0 +1,16 @@
+package com.codegym.controller;
+
+import com.codegym.model.Category;
+import com.codegym.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+}
