6e15db4e7b9a47f899872943329170b4ece3a732
vietson6103@gmail.com <Hn0912388240>
2018-09-22 11:29:18 +0700

    add method edit a category in class CategoryController

--- src/main/java/com/codegym/controller/CategoryController.java
+++ src/main/java/com/codegym/controller/CategoryController.java
@@ -4,10 +4,7 @@ import com.codegym.model.Category;
 import com.codegym.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -37,6 +34,28 @@ public class CategoryController {
         ModelAndView modelAndView = new ModelAndView("/category/create");
         modelAndView.addObject("category", new Category());
         modelAndView.addObject("message", "New category created successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/edit-category/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Category category = categoryService.findById(id);
+        if (category != null) {
+            ModelAndView modelAndView = new ModelAndView("/category/edit");
+            modelAndView.addObject("category", category);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/category/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-category")
+    public ModelAndView updateCategory(@ModelAttribute("category") Category category) {
+        categoryService.save(category);
+        ModelAndView modelAndView = new ModelAndView("/category/edit");
+        modelAndView.addObject("category", category);
+        modelAndView.addObject("message", "This product has been up to date successfully");
         return modelAndView;
     }
 }
