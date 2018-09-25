117f97b97cfe49b7a789a2c193b3c83a780f08f1
vietson6103@gmail.com <Hn0912388240>
2018-09-25 13:54:44 +0700

    edit class HomeController

---
+++ src/main/java/com/codegym/controller/HomeController.java
@@ -0,0 +1,45 @@
+package com.codegym.controller;
+
+import com.codegym.model.Category;
+import com.codegym.model.Producer;
+import com.codegym.model.Product;
+import com.codegym.service.CategoryService;
+import com.codegym.service.ProducerService;
+import com.codegym.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private ProducerService producerService;
+
+    @ModelAttribute("categories")
+    public Iterable<Category> categories() {
+        return categoryService.findAll();
+    }
+
+    @ModelAttribute
+    public Iterable<Producer> producers() {
+        return producerService.findAll();
+    }
+
+    @GetMapping("/")
+    public ModelAndView listHome(Pageable pageable) {
+        Page<Product> products = productService.findAll(pageable);
+        ModelAndView modelAndView = new ModelAndView("/home");
+        modelAndView.addObject("products", products);
+        return modelAndView;
+    }
+}
